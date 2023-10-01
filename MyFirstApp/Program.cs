c77d877baab5b7dd6f01562b78ab05459ec5c4c1
Priit P. <priit.printsmann@tktk.ee>
2023-10-01 11:47:38 +0300

    Create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //output
+//string - sõne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+
+//stringinterpolation
+Console.WriteLine($"Hello, {userName}!"); //output
